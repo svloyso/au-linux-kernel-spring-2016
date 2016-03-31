895022e57d935868f8dd7be85829ac342f62c992
svloyso <svloyso@gmail.com>
2016-03-31 17:33:12 +0300

    cw1

--- tasks/virt_mem_max/main.c
+++ tasks/virt_mem_max/main.c
@@ -1,5 +1,21 @@
+#include <sys/mman.h>
+#include <malloc.h>
+#include <stdio.h>
+
 int main()
 {
-    // TODO
+    long long req = 128L * 1024 * 1024 * 1024 * 1024;
+    long long got = 0;
+    int allocations = 0;
+    while(req > 1024) {
+        allocations = 0;
+        while((void*) -1 != mmap(NULL, req, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0)) {
+            allocations += 1;
+            got += req;
+        }
+        printf("%lld bytes allocations: %d\n", req, allocations);
+        req /= 2;
+    }
+    printf("%lld bytes allocated at all\n", got);
     return 0;
 }
